f3f3cec52f7f11f7b2f6617cccf7cb1765d67dbe
EXJANNA <32314962+YuangLiu00420@users.noreply.github.com>
2018-01-11 22:10:40 -0500

    localstorage

---
+++ js/starter.js
@@ -0,0 +1,6 @@
+let contactStarter = [
+    {"fullname":"Yuang Liu","phone":"613-407-9621","email":"ryanliu0203@gmail.com"},
+//    {"fullname":"Han Solo","phone":"613-727-4723","email":"han.solo@falcon.com"},
+//    {"fullname":"Luke Skywalker","phone":"613-747-4922","email":"luke.skywalker@jedi.org"},
+//    {"fullname":"Kylo Ren","phone":"613-997-6423","email":"kylo.ren@firstorder.io"}
+];
